817a7b9e0c5bc0dea0d1af8dec922c139cfac4b4
jgum63 <joshgum3@gmail.com>
2023-06-05 13:04:49 -0500

    updated components to reflect proper names

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
+import Resume from './pages/Resume';
 import About from './pages/About';
-import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // TODO: Add a comment describing the functionality of this method
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
     }
     return <Contact />;
   };
